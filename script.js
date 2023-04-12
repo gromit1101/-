11268d220387b6cd522f352d62fffe66307a9f1d
gromit1101 <78224810+gromit1101@users.noreply.github.com>
2023-04-12 21:09:32 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const profiles = [
+    {
+      id: 1,
+      name: "김민지",
+      age: 26,
+      location: "서울",
+      job: "디자이너",
+      introduction: "안녕하세요. 저는 디자인을 좋아하는 김민지입니다. 자연과 함께하는 여행을 즐기며 디자인 아이디어를 얻는 것을 좋아합니다.",
+      image: "https://source.unsplash.com/400x400/?young,korean"
+    },
+    {
+      id: 2,
+      name: "이승준",
+      age: 28,
+      location: "부산",
+      job: "개발자",
+      introduction: "안녕하세요. 웹 개발을 좋아하는 이승준입니다. 새로운 기술에 관심이 많아 공부하는 것을 좋아합니다.",
+      image: "https://source.unsplash.com/400x400/?young,korean"
+    },
+    {
+      id: 3,
+      name: "박하나",
+      age: 24,
+      location: "대구",
+      job: "대학생",
+      introduction: "안녕하세요. 대학에서 영문학을 전공하는 박하나입니다. 영화와 음악을 좋아하며 책을 읽는 것을 즐깁니다.",
+      image: "https://source.unsplash.com/400x400/?young,korean"
+    },
+    {
+      id: 4,
+      name: "김준형",
+      age: 25,
+      location: "인천",
+      job: "경영학과 대학원생",
+      introduction: "안녕하세요. 경영학과 대학원생인 김준형입니다. 노래를 부르는 것을 좋아하며 축구와 농구를 즐깁니다.",
+      image: "https://source.unsplash.com/400x400/?young,korean"
+    },
+  ];
+  
